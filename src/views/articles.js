b5d1500bbadeeaaedb0aa402707b87179c117187
tomaszpankowski <--get-all>
2022-05-23 08:45:12 +0200

    layout

--- src/views/articles.js
+++ src/views/articles.js
@@ -1,11 +1,19 @@
-import {Container} from "react-bootstrap";
+import React,{Component} from "react";
+import Container from "../../node_modules/react-bootstrap/Container";
+import ArticlesS1 from "../components/articles-s1";
+import ArticlesS2 from "../components/articles-s2";
+import ArticlesS3 from "../components/articles-s3";
 
-function Articles(props){
-    return(
-        <Container fluid className="minh-footer-adj bg-dark d-flex align-items-center py-5">
-            
-        </Container>
-    );
+class Articles extends Component{
+    render(){
+        return(        
+            <Container fluid className="minh-footer-adj p-0">
+                <ArticlesS1/>
+                <ArticlesS2/>
+                <ArticlesS3/>
+            </Container>    
+        );
+    }
 }
 
 export default Articles;
